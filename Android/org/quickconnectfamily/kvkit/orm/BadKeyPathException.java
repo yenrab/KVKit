384c7fc2201320a58bafa5720646869427043eff
Lee Barney <barney.cit@gmail.com>
2012-12-14 10:02:36 -0700

    added KeyPathException and forcing a file name change for DeletedStorable and InvalidValue exceptions

---
+++ Android/org/quickconnectfamily/kvkit/orm/BadKeyPathException.java
@@ -0,0 +1,25 @@
+package org.quickconnectfamily.kvkit.orm;
+
+@SuppressWarnings("serial")
+public class BadKeyPathException extends Exception {
+
+	public BadKeyPathException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadKeyPathException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadKeyPathException(Throwable throwable) {
+		super(throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadKeyPathException(String detailMessage, Throwable throwable) {
+		super(detailMessage, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
